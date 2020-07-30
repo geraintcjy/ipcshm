4d099a2638ef595bfb3d731a11b41aeac1399747
geraint <geraintcjy@gmail.com>
2020-07-30 11:01:09 +0800

    0730

--- algorithm/getData.py
+++ algorithm/getData.py
@@ -2,9 +2,53 @@ import hdf5storage
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""
+num代表第几天，应为0-30
+csv最大列数16384，只能把72000放在行上
+getDayData规模为72000*912，每列为一个小时的传感器数据
+getDayLabel规模为1*912，每个数为一个小时的标签
+"""
 
-def getDayData(num):
-    # getDayData获取date一天24小时的数据
+length = 15  # 数据简化的步长
+
+
+def simplifier(data, length):
+    # 取每length个数中绝对值最大的
+    answer = np.zeros((912, 72000 // length))
+    for i in range(912):
+        maxnum = 0.  # 记录最大值
+        count = 0  # 控制length
+        new_column = np.zeros(72000 // length)
+
+        while count <= 72000:
+            if count % length == 0 and count > 0:
+                if count == 72000:
+                    pass
+                else:
+                    try:
+                        if abs(maxnum) < abs(data[count][i]):
+                            maxnum = data[count][i]
+                    except RuntimeError:
+                        if str(data[count][i]) == 'NaN':
+                            data[count][i] = 0
+
+                new_column[count // length - 1] = maxnum
+                maxnum = 0.
+                count += 1
+            else:
+                try:
+                    if abs(maxnum) < abs(data[count][i]):
+                        maxnum = data[count][i]
+                except RuntimeError:
+                    if str(data[count][i]) == 'NaN':
+                        data[count][i] = 0
+                count += 1
+
+        answer[i] = new_column
+    return answer.T
+
+
+def getDayData(num, simplify):
     """
     列次序：
     00:00-01:00的1,2,3,4,5...测点数据,
@@ -19,11 +63,14 @@ def getDayData(num):
     data = np.zeros((72000, 912))
     for hour in range(24):
         if hour < 10:
-            datatemp = hdf5storage.loadmat('../'+date + '/' + date + ' 0' + str(hour) + '-VIB.mat')['data']
+            datatemp = hdf5storage.loadmat('../' + date + '/' + date + ' 0' + str(hour) + '-VIB.mat')['data']
         else:
-            datatemp = hdf5storage.loadmat('../'+date + '/' + date + ' ' + str(hour) + '-VIB.mat')['data']
+            datatemp = hdf5storage.loadmat('../' + date + '/' + date + ' ' + str(hour) + '-VIB.mat')['data']
         for j in range(38):
             data[:, hour * 38 + j] = datatemp[:, j]
+
+    if simplify:
+        data = simplifier(data, length)
 
     return data
 
@@ -32,16 +79,8 @@ def getLabel():
     return hdf5storage.loadmat('../label_r.mat')['manual_r']
 
 
-"""
-num代表第几天，应为0-30
-csv最大列数16384，只能把72000放在行上
-wholeData规模为72000*912，每列为一个小时的传感器数据
-wholeLabel规模为1*912，每个数为一个小时的标签
-"""
-
-
 def getDayLabel(num):
-    wholeLabel = np.zeros((1, 912))  # 完整的标签
+    wholeLabel = np.zeros((1, 912))
     label = getLabel()
     for j in range(24):
         for k in range(38):
@@ -50,19 +89,17 @@ def getDayLabel(num):
 
 
 if __name__ == '__main__':
-    data = getDayData(1)
+    data = getDayData(1, True)
     label = getDayLabel(1)
     print(data)
     print(label)
     print(data.shape)
     print(label.shape)
 
-    """
     # plot
-    x = np.linspace(1, 3600, 72000)
+    x = np.linspace(1, 3600, 72000 // length)
     y = []
-    for rows in data[0]:
+    for rows in data:
         y.append(rows[0])
-    plt.scatter(x, y)
+    plt.plot(x, y)
     plt.show()
-    """
--- algorithm/toCSV.py
+++ algorithm/toCSV.py
@@ -1,11 +1,12 @@
 import getData
 import csv
 
+
 with open('../input/training_data.csv', encoding='utf-8', mode='w', newline='') as file1:
     writer = csv.writer(file1)
     for i in range(1):
         print('Get Training Data: Doing day', str(i))
-        dayData = getData.getDayData(i)
+        dayData = getData.getDayData(i, True)
         writer.writerows(dayData)
 
 with open('../input/training_labels.csv', encoding='utf-8', mode='w', newline='') as file2:
@@ -14,12 +15,12 @@ with open('../input/training_labels.csv', encoding='utf-8', mode='w', newline=''
         print('Get Training Label: Doing day', str(i))
         dayLabel = getData.getDayLabel(i)
         writer.writerows(dayLabel)
-'''
+
 with open('../input/test_data.csv', encoding='utf-8', mode='w', newline='') as file3:
     writer = csv.writer(file3)
     for i in [1]:
         print('Get Test Data: Doing day', str(i))
-        dayData = getData.getDayData(i)
+        dayData = getData.getDayData(i, True)
         writer.writerows(dayData)
 
 with open('../input/test_labels.csv', encoding='utf-8', mode='w', newline='') as file4:
@@ -28,4 +29,4 @@ with open('../input/test_labels.csv', encoding='utf-8', mode='w', newline='') as
         print('Get Test Label: Doing day', str(i))
         dayLabel = getData.getDayLabel(i)
         writer.writerows(dayLabel)
-'''
+
